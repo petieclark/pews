118c9a7af71851bae92a46ab0b2a2e5f1b400cda
Citadel <citadel@citadel.local>
2026-02-11 11:53:48 -0500

    fix: remove PWA service worker entirely - was causing infinite reload loop and browser crashes

--- web/static/sw.js
+++
@@ -1,9 +0,0 @@
-// Service worker disabled for demo - clear all caches and unregister
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((names) => Promise.all(names.map((n) => caches.delete(n))))
-      .then(() => self.clients.claim())
-      .then(() => self.registration.unregister())
-  );
-});
