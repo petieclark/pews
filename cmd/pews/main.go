5a4ba44a49c69d3a34d694bb07f198d13ac90f64
Citadel <citadel@citadel.local>
2026-02-11 13:47:05 -0500

    Fix build: comment out unwired worship routes, remove unused import

--- cmd/pews/main.go
+++ cmd/pews/main.go
@@ -38,7 +38,6 @@ import (
 	"github.com/petieclark/pews/internal/streaming"
 	"github.com/petieclark/pews/internal/tenant"
 	"github.com/petieclark/pews/internal/website"
-	"github.com/petieclark/pews/internal/worship"
 )
 
 func main() {
